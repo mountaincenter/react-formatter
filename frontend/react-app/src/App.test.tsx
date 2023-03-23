c0807acc60e03dfeeb9cce15dfea53b337e6f627
mountaincenter <y.hiroyuki560730@gmail.com>
2023-03-24 00:09:02 +0900

    create

--- frontend/react-app/src/App.test.tsx
+++ frontend/react-app/src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Appコンポーネント', () => {
+  it('レンダリングが正常に行われるか', () => {
+    const { getByText } = render(<App />);
+    const headingElement = getByText('Hello World!');
+    expect(headingElement).toBeInTheDocument();
+  });
 });
